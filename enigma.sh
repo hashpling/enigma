dc883efcd1be3b40ef9fd122ba981a0624d7d120
Charles Bailey <charles@hashpling.org>
2015-04-24 15:18:16 +0100

    First hack

---
+++ enigma.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+
+alphabet=ABCDEFGHIJKLMNOPQRSTUVWXYZ
+
+swap_fd () {
+	eval "exec {tmp}>&${!1} {$1}>&${!2}"
+	eval "exec {$2}>&$tmp {tmp}>&-"
+}
+
+open_files () {
+	for (( i=0; i != ${#alphabet}; i++ ))
+	do
+		letter=${alphabet:i:1}
+		eval "exec {orig$letter}>lamp$letter"
+	done
+}
+
+init_fds () {
+	for (( i=0; i != ${#alphabet}; i++ ))
+	do
+		letter=${alphabet:i:1}
+		eval "exec {fd$letter}>&\$orig$letter"
+	done
+}
+
+close_fds () {
+	for (( i=0; i != ${#alphabet}; i++ ))
+	do
+		letter=${alphabet:i:1}
+		eval "exec {fd$letter}>&-"
+	done
+}
+
+reflector () {
+	# Umkehrwalze B: AY BR CU DH EQ FS GL IP JX KN MO TZ VW
+	swap_fd fdA fdY
+	swap_fd fdB fdR
+	swap_fd fdC fdU
+	swap_fd fdD fdH
+	swap_fd fdE fdQ
+	swap_fd fdF fdS
+	swap_fd fdG fdL
+	swap_fd fdI fdP
+	swap_fd fdJ fdX
+	swap_fd fdK fdN
+	swap_fd fdM fdO
+	swap_fd fdT fdZ
+	swap_fd fdV fdW
+}
+
+move_to_tmp_fds () {
+	for (( i=0; i != ${#alphabet}; i++ ))
+	do
+		letter=${alphabet:i:1}
+		eval "exec {tmp$letter}>&\$fd$letter {fd$letter}>&-"
+	done
+}
+
+rotor_fwd () {
+	move_to_tmp_fds
+	for (( i=0; i != ${#alphabet}; i++ ))
+	do
+		letter1=${alphabet:i:1}
+		idx=$(expr index $alphabet ${1:(i+$2)%26:1})
+		letter2=${alphabet:(idx+25-$2)%26:1}
+		eval "exec {fd$letter2}>&\$tmp$letter1 {tmp$letter1}>&-"
+	done
+}
+
+rotor_rev () {
+	move_to_tmp_fds
+	for (( i=0; i != ${#alphabet}; i++ ))
+	do
+		letter1=${alphabet:i:1}
+		idx=$(expr index $alphabet ${1:(i+$2)%26:1})
+		letter2=${alphabet:(idx+25-$2)%26:1}
+		eval "exec {fd$letter1}>&\$tmp$letter2 {tmp$letter2}>&-"
+	done
+}
+
+scramble_outputs () {
+	init_fds
+	rotor_fwd "$1" "$4"
+	rotor_fwd "$2" "$5"
+	rotor_fwd "$3" "$6"
+	reflector
+	rotor_rev "$3" "$6"
+	rotor_rev "$2" "$5"
+	rotor_rev "$1" "$4"
+}
+
+rotor1=EKMFLGDQVZNTOWYHXUSPAIBRCJ
+rotor2=AJDKSIRUXBLHWTMCQGZNPYFVOE
+rotor3=BDFHJLCPRTXVZNYEIWGAKMUSQO
+rotor4=ESOVPZJAYQUIRHXLNFTGKDCMWB
+rotor5=VZBRGITYUPSDNHLXAWMJQOFECK
+
+r1pos=0
+r2pos=0
+r3pos=0
+
+next_pos () { (( r1pos=(r1pos+1)%26 )); }
+
+next () {
+	next_pos
+	scramble_outputs $rotor1 $rotor2 $rotor3\
+	                 $r1pos  $r2pos  $r3pos
+}
+
+encode () {
+	next
+	eval "printf '*' >&\$fd$1"
+	close_fds
+}
+
+open_files
+
+while read -n 1 ch
+do
+	expr index $alphabet "$ch" >/dev/null && encode "$ch"
+done
---
+++ lamp-board.c
@@ -0,0 +1,53 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/select.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void) {
+	int r, m = -1;
+	unsigned u;
+	int lamps[26];
+	char fname[13];
+	fd_set empty, reads;
+	struct timeval tv;
+	const struct timeval hour = {3600, 0};
+
+	for (u = 0; u < sizeof lamps / sizeof *lamps; u++) {
+		sprintf(fname, "lamp%c", 'A' + u);
+		lamps[u] = open(fname, O_RDONLY);
+		if (lamps[u] > m)
+			m = lamps[u];
+	}
+
+	FD_ZERO(&empty);
+
+	for (;;) {
+		FD_ZERO(&reads);
+		for (u = 0; u < sizeof lamps / sizeof *lamps; u++) {
+			if (lamps[u] != -1) {
+				FD_SET(lamps[u], &reads);
+			}
+		}
+		tv = hour;
+		r = select(m + 1, &reads, &empty, &empty, &tv);
+		if (r > 0) {
+			for (u = 0; u < sizeof lamps / sizeof *lamps; u++) {
+				if (FD_ISSET(lamps[u], &reads)) {
+					r = read(lamps[u], fname, sizeof(fname));
+					if (r == 0) {
+						close(lamps[u]);
+						sprintf(fname, "lamp%c", 'A' + u);
+						lamps[u] = open(fname, O_RDONLY);
+					} else {
+						putchar('A' + u);
+					}
+				}
+			}
+			fflush(stdout);
+		}
+	}
+	/* wot, no close()? */
+}
